f7d857ea1c5e2a3c63b0b17ace911d73ec50e03c
Brik <brikr@users.noreply.github.com>
2023-12-30 15:54:10 -0800

    show past raids

--- src/api/wowaudit.ts
+++ src/api/wowaudit.ts
@@ -47,20 +47,26 @@ export interface RaidsResponse {
 
 const API_BASE = "https://wowaudit.com";
 
-async function fetchWithCookie<T>(endpoint: string): Promise<T> {
+async function fetchWithCookie<T>(
+  endpoint: string,
+  init?: RequestInit
+): Promise<T> {
   const waKey = cookies().get("waKey")?.value;
   if (!waKey) {
     throw new Error("Invalid Wowaudit API Key");
   }
 
   const waResponse = await fetch(`${API_BASE}${endpoint}`, {
-    headers: { Authorization: waKey }
+    ...init,
+    headers: { ...init?.headers, Authorization: waKey }
   });
   return waResponse.json();
 }
 
 export async function getRaidList(): Promise<RaidsResponse> {
-  return fetchWithCookie("/v1/raids");
+  const params = new URLSearchParams();
+  params.set("include_past", "true");
+  return fetchWithCookie(`/v1/raids?${params}`);
 }
 
 export async function getRaid(id: string | number): Promise<Raid> {
--- src/app/raids/RaidSelector.tsx
+++ src/app/raids/RaidSelector.tsx
@@ -6,10 +6,30 @@ interface Props {
   raids: RaidWithoutDetails[];
 }
 
-export default async function RaidSelector({ raids }: Props) {
+// how many days of past/future raids will be shown
+const DAYS_IN_PAST = 14;
+const DAYS_IN_FUTURE = 14;
+
+function isDateInRange(date: string, start: string, end: string) {
+  return date >= start && date <= end;
+}
+
+export default function RaidSelector({ raids }: Props) {
+  const start = new Date();
+  start.setDate(start.getDate() - DAYS_IN_PAST);
+  const end = new Date();
+  end.setDate(end.getDate() + DAYS_IN_FUTURE);
+  const startStr = start.toISOString().substring(0, 10);
+  const endStr = end.toISOString().substring(0, 10);
+
+  // filter raids by start/end
+  const filteredRaids = raids.filter((raid) =>
+    isDateInRange(raid.date, startStr, endStr)
+  );
+
   return (
     <div className={styles.container}>
-      {raids.slice(0, 4).map((raid) => (
+      {filteredRaids.map((raid) => (
         <Link key={raid.id} href={`/raids/${raid.id}`}>
           <div className={styles.raid}>
             <span>{raid.date}</span>
